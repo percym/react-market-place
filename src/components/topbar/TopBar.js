132b28309e06461405c9db0689d8266f95d89f89
percy mugadza <percy.mugadza@ecocashholdings.co.zw>
2022-08-12 13:35:54 +0200

    styling top bar  done

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -6,26 +6,26 @@ function TopBar() {
     <div className='topbar'>
 
       <div className='topbar-left'>
-        <i className="fa-brands fa-instagram" />
-        <i className="fa-brands fa-facebook"></i>
-        <i className="fa-brands fa-square-twitter"></i>
-        <i className="fa-brands fa-linkedin"></i>
+        <i className="topbar-social-icon fa-brands fa-instagram" />
+        <i className="topbar-social-icon fa-brands fa-facebook"></i>
+        <i className="topbar-social-icon fa-brands fa-square-twitter"></i>
+        <i className="topbar-social-icon  fa-brands fa-linkedin"></i>
       </div>
 
       <div className='topbar-center'>
         
         <ul className='topbar-list '>
-          <li className='topbar-social topbar-list-item'>HOME</li>
-          <li className='topbar-social topbar-list-item'>ABOUT US</li>
-          <li className='topbar-social topbar-list-item'>CONTACT </li>
-          <li className='topbar-social topbar-list-item'>CREATE LISTING</li>
-          <li className='topbar-social topbar-list-item'>LOGOUT</li>
+          <li className='topbar-list-item'>HOME</li>
+          <li className='topbar-list-item'>ABOUT</li>
+          <li className='topbar-list-item'>CONTACT </li>
+          <li className='topbar-list-item'>CREATE LISTING</li>
+          <li className='topbar-list-item'>LOGOUT</li>
         </ul>
       </div>
 
       <div className='topbar-right'>
-        <img  className='topbar-profile' src='https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
-        <i className=" topbar-search topbar-social fa-solid fa-magnifying-glass-location"></i>
+        <img  className='topbar-profile-pic' src='https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
+        <i className="topbar-search  fa-solid fa-magnifying-glass-location"></i>
       </div>
 
     </div>
